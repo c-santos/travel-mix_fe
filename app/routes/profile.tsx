3d84451b3f26dc66c03b95597f58bfdb4e4f8a2e
c-santos <jcms121899@gmail.com>
2024-01-22 13:10:32 +0800

    Added profile load

--- app/routes/profile.tsx
+++ app/routes/profile.tsx
@@ -1,45 +1,102 @@
-import { LoaderFunctionArgs } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-import axios, { AxiosRequestConfig } from "axios";
-import React from "react"
+import { LoaderFunctionArgs, createCookie, createCookieSessionStorage } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import React from 'react';
 
-async function handleLoginCallback(request) {
-  const data = {
-    authCode: request.query.code
-  }
-  const options: AxiosRequestConfig = {
-    withCredentials: true,
-  }
-
-  try { 
-    const response = await axios.post(`${process.env.API_BASE_URL}/spotify-auth/callback`, data, options);
-    
-    // Sends POST request to backend to generate a user
-
-    // Response should contain cookie
-
-  } catch (error) {
-    console.error(error);
-  }
-
-  // Response should have cookie with the access token
+interface IToken {
+  access_token: string;
+  token_type: string;
+  scope: string;
+  expires_in: string;
+  refresh_token: string;
 }
 
-export async function loader({ request }) {
-  // Receive access token and store as cookie
-  await handleLoginCallback(request);
+async function getUserAccessToken(code: string) {
+  const config: AxiosRequestConfig = {
+    params: {
+      auth_code: code,
+    },
+  };
 
+  try {
+    // Sends GET request with auth_code as parameter to backend to generate an access token
+    const response = await axios.get(
+      `${process.env.API_BASE_URL}/spotify-auth/callback`,
+      config,
+    );
+    const accessToken: IToken = response.data;
 
+    return accessToken
+  } catch (error) {
+    console.error(error);
+    throw new Response('Could not generate access token.', {
+      status: 500,
+    })
+  }
+}
+
+async function getAuthCodeFromUrl(request: Request) {
+  const reqUrl: URL = new URL(request.url);
+  const code: string | null = reqUrl.searchParams.get('code');
+
+  if (!code) {
+    throw new Response('Authorization code not present in URL.', {
+      status: 500,
+    });
+  }
+
+  return code;
+}
+
+async function getProfile(accessToken: string) {
+  const response = await axios.get('https://api.spotify.com/v1/me', {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+  const profileData = response.data
+  return profileData;
+}
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const authCode = await getAuthCodeFromUrl(request);
+  const token = await getUserAccessToken(authCode);
+  const profile = await getProfile(token.access_token);
+
+  const data = {
+    name: profile.display_name,
+    url: profile.external_urls.spotify,
+    followers: profile.followers.total,
+    country: profile.country,
+    email: profile.email,
+    image: profile.images[0],
+  };
+
+  return data;
 }
 
 const Profile = () => {
-  // const data = useLoaderData<typeof loader>();
+  const data = useLoaderData<typeof loader>();
 
   return (
-    <div>
-      <h1> hello world </h1>
+    <div style={{ fontFamily: 'sans-serif', lineHeight: 1.8 }}>
+      <h1> Profile </h1>
+      <h2>{data.name}</h2>
+      <ul>
+        <li>
+          <a href={data.url}>Link to profile</a>
+        </li>
+        <li>followers: {data.followers}</li>
+        <li>{data.country}</li>
+        <li>{data.email}</li>
+        <img
+          src={data.image.url}
+          height={data.image.height}
+          width={data.image.width}
+        />
+      </ul>
     </div>
-  )
+  );
 };
 
 export default Profile;
