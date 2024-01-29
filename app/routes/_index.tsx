0869663de3439f6714bcbc50ce777678ec894007
c-santos <jcms121899@gmail.com>
2024-01-29 09:10:56 +0800

    Added redirect if user is already logged in

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,25 +2,52 @@ import type {
   ActionFunctionArgs,
   LoaderFunctionArgs,
   MetaFunction,
+  Session,
 } from '@remix-run/node';
-import { redirect } from '@remix-run/node';
+import { createCookieSessionStorage, redirect } from '@remix-run/node';
 import { Form, useLoaderData } from '@remix-run/react';
 import axios from 'axios';
-import { getSession } from '~/session';
+import { commitSession, getSession, isTokenExpired } from '@/session.server';
 
 export const meta: MetaFunction = () => {
   return [
-    { title: 'Remix App' },
+    { title: 'Travel Mix' },
     { name: 'description', content: 'Welcome to Remix!' },
   ];
 };
 
 export async function action({ request }: ActionFunctionArgs) {
-  // Login
-  const response = await axios.get('http://localhost:3005/spotify-auth/login');
-  const spotifyAuthUrl = response.data;
+  try {
+    const response = await axios.get(
+      'http://localhost:3005/spotify-auth/login',
+    );
+    const spotifyAuthUrl = response.data;
+    return redirect(spotifyAuthUrl);
+  } catch (error) {
+    throw new Error('Could not initiate login process', {
+      cause: error,
+    });
+  }
+}
 
-  return redirect(spotifyAuthUrl);
+export async function isLoggedIn(cookieHeader: string | null) {
+  let session = await getSession(cookieHeader);
+  const tokenExpiryDate = session.get('expires');
+
+  let isLoggedIn;
+
+  if (await isTokenExpired(tokenExpiryDate!)) {
+    isLoggedIn = false;
+  } else {
+    isLoggedIn = true;
+  }
+
+  return isLoggedIn;
+}
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const cookieHeader = request.headers.get('cookie');
+  return (await isLoggedIn(cookieHeader)) ? redirect('/me/tracks') : null;
 }
 
 export default function Index() {
